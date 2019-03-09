7f8230606a6c90d7090ddd61e0ec152d09068120
tisquip6 <tisquip6@gmail.com>
2019-03-09 23:30:07 +0200

    IAzureBlob interface added

---
+++ AzureBlobSample/Services/AzureBlob/IAzureBlob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace AzureBlobSample.Services.AzureBlob
+{
+    public interface IAzureBlob
+    {
+        Task<(bool wasSuccesful, Uri uploadUrl, string blobContainerReference)> UploadToBlob(IFormFile formfile, string fileName = "");
+        Task<(bool wasSuccesful, Uri uploadUrl, string blobContainerReference)> UploadToBlob(Stream stream, string fileName);
+        Task<bool> DeleteBlob(string blobContainerReference);
+    }
+}
